832a94847a6f02737493ada934f7f5dc71feb081
Qicz <qiczzhu@gmail.com>
2022-05-15 16:56:24 +0800

    add helloworld

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/openingo/godkits/gox/stringsx"
+)
+
+func main() {
+	helloworld := stringsx.DefaultIfEmpty("", "hello world")
+	fmt.Println(helloworld)
 }
