1de8c853d4461b9a64a68cd56ae0da2f14c49518
moremind <hefengen@hotmail.com>
2022-07-13 10:22:00 +0800

    [type:feat] add storage interface and plugin chain (#24)

---
+++ internal/core/store/storage.go
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2022, AcmeStack
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package store
+
+// Storage store interface
+type Storage interface {
+
+	// Put put data into storage
+	//  @param o data
+	Put(o interface{})
+
+	// Get get data from storage
+	//  @param o data
+	Get(o interface{})
+
+	// Find find data in storage
+	//  @param o data
+	Find(o interface{})
+
+	// Remove remove data from storage
+	//  @param o data
+	Remove(o interface{})
+}
