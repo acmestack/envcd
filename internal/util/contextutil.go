101087ee40c1819c7548d7a6816940c25d8aba84
moremind <hefengen@hotmail.com>
2022-08-01 14:40:02 +0800

    [type:feat] build context

--- internal/util/contextutil.go
+++ internal/util/contextutil.go
@@ -3,14 +3,27 @@ package util
 import (
 	"github.com/acmestack/envcd/internal/pkg/context"
 	"github.com/acmestack/envcd/pkg/entity/data"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
 )
 
 // BuildContext build plugin context
 //  @param params params
 //  @return *context.Context context
 //  @return error error
-func BuildContext(params interface{}) (*context.Context, error) {
-	// TODO build context
+func BuildContext(ginCtx *gin.Context) (*context.Context, error) {
+	ctx := &context.Context{
+		Uri:         ginCtx.Request.RequestURI,
+		Method:      ginCtx.Request.Method,
+		Headers:     buildContextHeaders(ginCtx),
+		ContentType: ginCtx.ContentType(),
+		Cookies:     buildContextCookies(ginCtx),
+		Body:        buildRequestBody(ginCtx),
+		HttpRequest: ginCtx.Request,
+	}
+	if ctx != nil {
+		return ctx, nil
+	}
 	return nil, nil
 }
 
@@ -21,4 +34,37 @@ func BuildContext(params interface{}) (*context.Context, error) {
 func ParseContext(ctx *context.Context) (*data.EnvcdData, error) {
 	// TODO parse context to envcd data
 	return nil, nil
+}
+
+// buildContextHeaders build plugin context headers
+//  @param ginCtx gin context
+//  @return map[string]interface{} ret
+func buildContextHeaders(ginCtx *gin.Context) map[string]interface{} {
+	maps := make(map[string]interface{})
+	for k, v := range ginCtx.Request.Header {
+		maps[k] = v
+	}
+	return maps
+}
+
+// buildContextCookies build plugin context cookies
+//  @param ginCtx gin context
+//  @return map[string]interface{} ret
+func buildContextCookies(ginCtx *gin.Context) map[string]interface{} {
+	maps := make(map[string]interface{})
+	for k, v := range ginCtx.Request.Cookies() {
+		maps[string(rune(k))] = v
+	}
+	return maps
+}
+
+// buildRequestBody build request body
+//  @param ginCtx gin context
+//  @return string request body
+func buildRequestBody(ginCtx *gin.Context) string {
+	all, err := ioutil.ReadAll(ginCtx.Request.Body)
+	if err != nil {
+		return ""
+	}
+	return string(all)
 }
